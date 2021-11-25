c82a4c51541eb3ccab30c569025c3e2424b0b219
jitasek <jitka.moreira@gmail.com>
2021-11-25 18:02:01 +0100

    add managerQuestions

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeam = require("./src/generateTeam");
 
 team = [];
+
+const managerQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's e-mail address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?",
+      },
+      {
+        type: "list",
+        name: "addTeamMember",
+        message: "Which team member/s would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more memebers.",
+        ],
+      },
+    ])
+    .then((managerAnswers) => {
+      console.log(managerAnswers);
+    });
+};
+managerQuestions();
--- src/generateTeam.js
+++ src/generateTeam.js
@@ -3,16 +3,16 @@ const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
 
 // function that generates card for Manager, Engineer and Intern
-function generateTeamCards(team) {
-  let cards = [];
-  // loop through team and generate relevant cards for each team member
-}
+//function generateTeamCards(team) {
+//let cards = [];
+// loop through team and generate relevant cards for each team member
+//}
 
 // function for generating Manager card
 // function for generating Engineer card
 // function for generating Intern card
 
 // function that generates the team
-function generateTeam(team) {
-  //console.log(team);
-}
+//function generateTeam(team) {
+//console.log(team);
+//}
