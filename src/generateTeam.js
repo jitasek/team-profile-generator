58c987f345f6cdb7b392fd5efe2032491c48a95d
jitasek <jitka.moreira@gmail.com>
2021-11-25 17:17:33 +0100

    prep generateTeam.js

---
+++ src/generateTeam.js
@@ -0,0 +1,18 @@
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+
+// function that generates card for Manager, Engineer and Intern
+function generateTeamCards(team) {
+  let cards = [];
+  // loop through team and generate relevant cards for each team member
+}
+
+// function for generating Manager card
+// function for generating Engineer card
+// function for generating Intern card
+
+// function that generates the team
+function generateTeam(team) {
+  //console.log(team);
+}
